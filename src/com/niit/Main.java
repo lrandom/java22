6d1ce9583385c3d04327ca57ef0e28fbb82e0273
Nguyen Thanh Luan <luann4099@gmail.com>
2021-09-23 19:54:59 +0700

    demo scanner

--- src/com/niit/Main.java
+++ src/com/niit/Main.java
@@ -1,15 +1,21 @@
 package com.niit;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int myAge = 20;
+        /*int myAge = 20;
         String name = "Nam";
 
         System.out.println("My age is "+myAge); //My age is 20
         System.out.printf("My age is %d %n",myAge);//My age is 20
         System.out.printf("The float is %.3f %n", 0.003);
-        System.out.printf("The float is %s",name);
+        System.out.printf("The float is %s",name);*/
 
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Vui long cho biet so tuoi cua ban");
+        int myAge =  myScanner.nextInt();
+        System.out.println("So tuoi cua ban la " + myAge);
     }
 }
