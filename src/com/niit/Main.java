fdfa5896df25ee70578c7d461da51d46b8a8bd70
Nguyen Thanh Luan <luann4099@gmail.com>
2021-09-23 19:45:36 +0700

    session 1

---
+++ src/com/niit/Main.java
@@ -0,0 +1,15 @@
+package com.niit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myAge = 20;
+        String name = "Nam";
+
+        System.out.println("My age is "+myAge); //My age is 20
+        System.out.printf("My age is %d %n",myAge);//My age is 20
+        System.out.printf("The float is %.3f %n", 0.003);
+        System.out.printf("The float is %s",name);
+
+    }
+}
