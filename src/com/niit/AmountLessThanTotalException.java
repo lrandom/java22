951dc9f66b96da9552d4e58ed6645bb433ffa6ad
Nguyen Thanh Luan <luann4099@gmail.com>
2021-10-19 20:27:19 +0700

    demo write/read

--- src/com/niit/AmountLessThanTotalException.java
+++
@@ -1,7 +0,0 @@
-package com.niit;
-
-public class AmountLessThanTotalException extends Exception {
-    public AmountLessThanTotalException() {
-        super("Vui long nhap so tien nho hon so tien trong cay ATM");
-    }
-}
--- src/com/niit/Main.java
+++ src/com/niit/Main.java
@@ -1,75 +1,54 @@
 package com.niit;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      /*  int num = 10;
-        int arr[] = new int[]{1, 3, 4};
-
-        try {
-            num = arr[5];
-        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
-            //indexOutOfBoundsException.printStackTrace();
-            System.out.println("Lỗi truy cập phần tử vuợt quá độ dài mảng");
-        } catch (Exception exception) {
-            System.out.println("Lỗi " + exception.getMessage());
-        } finally {
-            System.out.println("Finally");
-        }
-        System.out.println("Hello");*/
-
-/*        Employee employee = new Employee();
-        try {
-            employee.setAge(-1);
-        } catch (RuntimeException e) {
-            System.out.println("Tuoi khong the nho hon 0");
-        } catch (Exception e) {
-            System.out.println("Tuoi khong the lon hon 120");
-            System.out.println("Vui long nhap lai tuoi");
-        }*/
-
-        //try with resources
-/*        try (FileInputStream fis = new FileInputStream("note.txt")) {
-            //doc file o cho nay
-            int i = 0;
-            while ((i = fis.read()) != -1) {
-                System.out.println((char) i);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Vui long dua vao dung duong dan file");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream("note.txt");
-            int i = 0;
-            while ((i = fis.read()) != -1) {
-                System.out.println((char) i);
+        /*FileInputStream fis;
+        BufferedInputStream bufferedInputStream;
+        FileReader fileReader;
+        BufferedReader bufferedReader;
+        int i = 0;*/
+/*        try {
+            //win - fis = new FileInputStream("C:\\Users\\lrandom\\Desktop/note.txt");
+            fis = new FileInputStream("/Users/lrandom/Desktop/note.txt");
+            bufferedInputStream = new BufferedInputStream(fis);
+            while ((i = bufferedInputStream.read()) != -1) {
+                System.out.print((char) i);
             }
             fis.close();
+            bufferedInputStream.close();
         } catch (FileNotFoundException e) {
-            System.out.println("Vui long dua vao dung duong dan file");
-        } catch (IOException e) {
             e.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
         }*/
 
-        Scanner scanner = new Scanner(System.in);
-        ATM atm = new ATM();
-        System.out.println("Vui long nhap so tien ban muon rut");
-        float amount = scanner.nextFloat();
+       /* try {
+            fileReader = new FileReader("/Users/lrandom/Desktop/note.txt");
+            bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while ((line=bufferedReader.readLine())!=null) {
+                System.out.print(line);
+            }
+            fileReader.close();
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }*/
+
+
+        FileOutputStream fos;
+
         try {
-            atm.withdraw(amount);
-        } catch (AmountLessThanTotalException amountLessThanTotalException) {
-            System.out.println("Vui long nhap lai so tin nho hon trong tk");
+            fos = new FileOutputStream("/Users/lrandom/Desktop/note.txt",false);
+            fos.write(new String("ABCDEF").getBytes());
+            fos.close();
+        } catch (Exception e) {
+
         }
-
-
     }
 }
